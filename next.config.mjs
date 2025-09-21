b9cc1661c8d9043348ec96f056b67099c909f55f
eliezerriveralopez07-lab <eliezer.riveralopez07@gmail.com>
2025-09-21 19:17:15 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,3 @@
-// next.config.mjs
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-
-  // Make sure Next doesn't try to bundle the DB driver for RSC/server routes.
-  // This fixes "Can't resolve 'postgres'" when building on Vercel.
-  experimental: {
-    serverComponentsExternalPackages: ['postgres'],
-  },
+export default {
+  experimental: { serverComponentsExternalPackages: ['postgres'] },
 };
-
-export default nextConfig;
