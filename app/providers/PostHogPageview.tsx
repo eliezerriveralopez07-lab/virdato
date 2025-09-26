6f3c9ab3a547f17c291d14f14f1db3c1b85f9871
VIRDATO <eliezer.riveralopez07@gmail.com>
2025-09-26 13:53:18 -0400

    Create PostHogPageview.tsx

---
+++ app/providers/PostHogPageview.tsx
@@ -0,0 +1,14 @@
+// app/providers/PostHogPageview.tsx
+'use client'
+import { usePathname, useSearchParams } from 'next/navigation'
+import { useEffect } from 'react'
+import posthog from 'posthog-js'
+
+export default function PostHogPageview() {
+  const pathname = usePathname()
+  const search = useSearchParams()
+  useEffect(() => {
+    if (pathname) posthog.capture('$pageview')
+  }, [pathname, search])
+  return null
+}
