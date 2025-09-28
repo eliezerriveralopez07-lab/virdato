7363e0e3bf843e57e2495b20fae635e8c9f860b8
VIRDATO <eliezer.riveralopez07@gmail.com>
2025-09-28 08:13:19 -0400

    Update PostHogPageview.tsx

--- app/providers/PostHogPageview.tsx
+++ app/providers/PostHogPageview.tsx
@@ -1,14 +1,17 @@
-'use client'
-import { usePathname, useSearchParams } from 'next/navigation'
-import { useEffect } from 'react'
-import posthog from 'posthog-js'
+'use client';
+
+import { useEffect } from 'react';
+import { usePathname, useSearchParams } from 'next/navigation';
+import posthog from 'posthog-js';
 
 export default function PostHogPageview() {
-  const pathname = usePathname()
-  const search = useSearchParams()
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
   useEffect(() => {
-    if (pathname) posthog.capture('$pageview')
-  }, [pathname, search])
-  return null
+    posthog.capture('$pageview');
+  }, [pathname, searchParams]);
+
+  return null;
 }
 
