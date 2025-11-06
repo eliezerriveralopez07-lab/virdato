72415dc789338e31f2cf1b5ec3b971043eb9c0ac
VIRDATO <eliezer.riveralopez07@gmail.com>
2025-11-06 09:56:19 -0500

    Update VirdatoToken.sol

--- src/VirdatoToken.sol
+++ src/VirdatoToken.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.8.24;
 
 import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import {ERC20Permit} from "openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {ERC20Pausable} from "openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
-import {Pausable} from "openzeppelin-contracts/contracts/utils/Pausable.sol";
 
-contract VirdatoToken is ERC20, ERC20Permit, Ownable, Pausable {
+contract VirdatoToken is ERC20, ERC20Permit, ERC20Pausable, Ownable {
     constructor(address owner_, uint256 initialSupply)
         ERC20("Virdato", "VIRD")
         ERC20Permit("Virdato")
@@ -15,14 +15,7 @@ contract VirdatoToken is ERC20, ERC20Permit, Ownable, Pausable {
         _mint(owner_, initialSupply);
     }
 
+    // Owner-controlled pause/unpause (ERC20Pausable handles blocking transfers)
     function pause() external onlyOwner { _pause(); }
     function unpause() external onlyOwner { _unpause(); }
-
-    function _beforeTokenTransfer(address from, address to, uint256 amount)
-        internal
-        override
-    {
-        require(!paused(), "VIRD: paused");
-        super._beforeTokenTransfer(from, to, amount);
-    }
 }
