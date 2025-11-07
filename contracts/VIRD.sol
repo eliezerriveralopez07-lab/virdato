b604d60ad2a3c8d33c07b4fc7846295eb143d8ee
VIRDATO <eliezer.riveralopez07@gmail.com>
2025-11-07 12:23:41 -0500

    Update VIRD.sol

--- contracts/VIRD.sol
+++ contracts/VIRD.sol
@@ -1,1 +1,15 @@
+cat > contracts/VIRD.sol << 'EOF'
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {ERC20}  from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract VIRD is ERC20, Ownable {
+    constructor() ERC20("Virdato", "VIRD") Ownable(msg.sender) {
+        _mint(msg.sender, 1_000_000 ether);
+    }
+    function mint(address to, uint256 amount) external onlyOwner { _mint(to, amount); }
+}
+EOF
 
