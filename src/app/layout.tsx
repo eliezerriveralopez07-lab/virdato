f931d8f30edf0b6afd430b008846f2e5ae5c4fa7
VIRDATO <eliezer.riveralopez07@gmail.com>
2025-11-08 13:01:08 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,8 @@
-import Script from "next/script";
-import Providers from "./providers";
-
+export const metadata = { title: "VIRD" };
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head />
-      <body>
-        <Script
-          id="termly-cmp"
-          strategy="beforeInteractive"
-          src={`https://app.termly.io/resource-blocker/${process.env.NEXT_PUBLIC_TERMLY_WEBSITE_UUID}?autoBlock=on`}
-        />
-        <Providers>{children}</Providers>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
