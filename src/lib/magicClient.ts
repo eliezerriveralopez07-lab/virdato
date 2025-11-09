1c3571a8390548d2cbddd638e84a7f40e649216d
VIRDATO <eliezer.riveralopez07@gmail.com>
2025-11-09 10:08:19 -0500

    Update magicClient.ts

--- src/lib/magicClient.ts
+++ src/lib/magicClient.ts
@@ -1,8 +1,4 @@
 import { Magic } from "magic-sdk";
-
 export const magic = new Magic(process.env.NEXT_PUBLIC_MAGIC_PUBLISHABLE_KEY!, {
-  network: {
-    rpcUrl: process.env.NEXT_PUBLIC_RPC_URL!,
-    chainId: 80002, // Polygon Amoy
-  },
+  network: { rpcUrl: process.env.NEXT_PUBLIC_RPC_URL!, chainId: 80002 },
 });
